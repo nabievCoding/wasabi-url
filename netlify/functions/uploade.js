f5b73d9c652cffbb3a1b79c3734b6d38dcc93840
NabievCoding <nabievofficial04@gmail.com>
2025-09-13 23:21:21 +0500

    Update uploade.js

--- netlify/functions/uploade.js
+++ netlify/functions/uploade.js
@@ -1,44 +1,39 @@
+const AWS = require("aws-sdk");
+
+const s3 = new AWS.S3({
+  endpoint: "https://s3.ap-southeast-1.wasabisys.com",
+  region: "ap-southeast-1",
+  accessKeyId: process.env.WASABI_ACCESS_KEY,
+  secretAccessKey: process.env.WASABI_SECRET_KEY,
+  signatureVersion: "v4",
+});
+
 exports.handler = async (event) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  };
+  try {
+    const { fileName } = event.queryStringParameters;
 
-  const { fileName } = event.queryStringParameters || {};
+    const params = {
+      Bucket: process.env.WASABI_BUCKET_NAME,
+      Key: `uploads/${Date.now()}-${fileName}`,
+      Expires: 60,
+      ACL: "public-read",
+      ContentType: "application/octet-stream",
+    };
 
-  if (!fileName) {
+    const uploadUrl = await s3.getSignedUrlPromise("putObject", params);
+
     return {
-      statusCode: 400,
-      headers,
-      body: JSON.stringify({ error: "fileName kerak" }),
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        uploadUrl,
+        fileUrl: `https://${process.env.WASABI_BUCKET_NAME}.s3.ap-southeast-1.wasabisys.com/${params.Key}`,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ success: false, error: err.message }),
     };
   }
-
-  // Faylni bucket ichida saqlash nomi
-  const finalName = `uploads/${Date.now()}-${fileName}`;
-
-  const s3 = new AWS.S3({
-    endpoint: "https://s3.ap-southeast-1.wasabisys.com",
-    accessKeyId: process.env.WASABI_ACCESS_KEY,
-    secretAccessKey: process.env.WASABI_SECRET_KEY,
-    region: "ap-southeast-1",
-  });
-
-  const params = {
-    Bucket: process.env.WASABI_BUCKET_NAME,
-    Key: finalName,
-    Expires: 60,
-    ACL: "public-read",
-    ContentType: "application/octet-stream",
-  };
-
-  const uploadUrl = s3.getSignedUrl("putObject", params);
-  const fileUrl = `https://${process.env.WASABI_BUCKET_NAME}.s3.ap-southeast-1.wasabisys.com/${finalName}`;
-
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify({ uploadUrl, fileUrl }),
-  };
 };
